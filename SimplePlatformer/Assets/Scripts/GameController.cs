5ed28369c78dc6a4c97681eebddd44ef63a156b4
Vakor <vakor.dev@gmail.com>
2022-11-11 18:10:12 +0200

    Created tiles and TileText

--- SimplePlatformer/Assets/Scripts/GameController.cs
+++ SimplePlatformer/Assets/Scripts/GameController.cs
@@ -1,30 +1,47 @@
+using System;
 using Games;
 using Mazes;
+using TMPro;
 using UnityEngine;
+
+[Serializable]
+public class Tile
+{
+    public GameObject tileSquare;
+    public TMP_Text tileText;
+}
 
 public class GameController : MonoBehaviour
 {
     // Start is called before the first frame update
-    public GameObject tile;
+    public Tile tile;
     public float distanceBtwTiles;
     private Maze _maze;
+    private Tile[,] _tiles;
     
     void Start()
     {
         _maze = Maze.GenerateDefaultMaze();
+        _tiles = new Tile[_maze.Height,_maze.Width];
         InstantiateField();
     }
 
     private void InstantiateField()
     {
+        
         for (int i = 0; i < _maze.Height; i++)
         {
             for (int j = 0; j < _maze.Width; j++)
             {
                 if (_maze[i,j] == 1)
                 {
-                    Instantiate(tile, new Vector3(distanceBtwTiles * i, 0, distanceBtwTiles * j),
-                        Quaternion.identity);
+                    _tiles[i, j] = new Tile
+                    {
+                        tileSquare = Instantiate(tile.tileSquare, new Vector3(distanceBtwTiles * i, 0, distanceBtwTiles * j),
+                            Quaternion.identity),
+                        tileText = (TMP_Text)Instantiate(tile.tileText, new Vector3(distanceBtwTiles * i, 0, distanceBtwTiles * j),
+                            Quaternion.identity).GetComponent(typeof(TMP_Text))
+                    };
                 }
             }
         }
