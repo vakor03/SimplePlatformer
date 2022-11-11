d900f0c1f1a61fc70349236ca6a267e82d51dad7
Vakor <vakor.dev@gmail.com>
2022-11-11 17:59:29 +0200

    Added text to tiles

--- SimplePlatformer/Assets/Scripts/GameController.cs
+++ SimplePlatformer/Assets/Scripts/GameController.cs
@@ -1,13 +1,33 @@
-using System.Collections;
-using System.Collections.Generic;
+using Games;
+using Mazes;
 using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
     // Start is called before the first frame update
+    public GameObject tile;
+    public float distanceBtwTiles;
+    private Maze _maze;
+    
     void Start()
     {
-        
+        _maze = Maze.GenerateDefaultMaze();
+        InstantiateField();
+    }
+
+    private void InstantiateField()
+    {
+        for (int i = 0; i < _maze.Height; i++)
+        {
+            for (int j = 0; j < _maze.Width; j++)
+            {
+                if (_maze[i,j] == 1)
+                {
+                    Instantiate(tile, new Vector3(distanceBtwTiles * i, 0, distanceBtwTiles * j),
+                        Quaternion.identity);
+                }
+            }
+        }
     }
 
     // Update is called once per frame
