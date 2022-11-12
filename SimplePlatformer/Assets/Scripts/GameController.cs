7e39e1ce56ee10ede93a4baea19474410638ac58
Vakor <vakor.dev@gmail.com>
2022-11-12 09:29:38 +0200

    Simple path generation created

--- SimplePlatformer/Assets/Scripts/GameController.cs
+++ SimplePlatformer/Assets/Scripts/GameController.cs
@@ -1,10 +1,13 @@
 using System;
+using System.Collections.Generic;
+using Additional;
 using Games;
 using Mazes;
+using PathfindingAlgorithms;
 using TMPro;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
-[Serializable]
 public class Tile
 {
     public GameObject tileSquare;
@@ -14,42 +17,71 @@ public class Tile
 public class GameController : MonoBehaviour
 {
     // Start is called before the first frame update
-    public Tile tile;
+
+    public GameObject tile;
     public float distanceBtwTiles;
     private Maze _maze;
     private Tile[,] _tiles;
-    
+    private IPathFindingAlgorithm _pathFindingAlgorithm;
+
     void Start()
     {
         _maze = Maze.GenerateDefaultMaze();
-        _tiles = new Tile[_maze.Height,_maze.Width];
+        _tiles = new Tile[_maze.Height, _maze.Width];
+        _pathFindingAlgorithm = new LeeAlgorithm();
         InstantiateField();
+        SpawnEnemy();
     }
 
     private void InstantiateField()
     {
-        
         for (int i = 0; i < _maze.Height; i++)
         {
             for (int j = 0; j < _maze.Width; j++)
             {
-                if (_maze[i,j] == 1)
+                if (_maze[i, j] == 1)
                 {
+                    GameObject tileSquare = Instantiate(tile,
+                        new Vector3(distanceBtwTiles * i, 0, distanceBtwTiles * j),
+                        Quaternion.identity);
                     _tiles[i, j] = new Tile
                     {
-                        tileSquare = Instantiate(tile.tileSquare, new Vector3(distanceBtwTiles * i, 0, distanceBtwTiles * j),
-                            Quaternion.identity),
-                        tileText = (TMP_Text)Instantiate(tile.tileText, new Vector3(distanceBtwTiles * i, 0, distanceBtwTiles * j),
-                            Quaternion.identity).GetComponent(typeof(TMP_Text))
+                        tileSquare = tileSquare,
+                        tileText = tileSquare.GetComponentInChildren<TMP_Text>()
                     };
+                    _tiles[i, j].tileText.text = "";
                 }
             }
+        }
+    }
+
+    private void SpawnEnemy()
+    {
+        Coordinates enemyCoordinates = new Coordinates(-1, -1);
+        while (!_maze.CheckCoordinatesForValid(enemyCoordinates))
+        {
+            enemyCoordinates = new Coordinates(Random.Range(0, _maze.Height), Random.Range(0, _maze.Width));
+        }
+
+        _tiles[enemyCoordinates.X, enemyCoordinates.Y].tileText.text = "S";
+
+        Coordinates enemyCoordinates1 = new Coordinates(-1, -1);
+        while (!_maze.CheckCoordinatesForValid(enemyCoordinates1))
+        {
+            enemyCoordinates1 = new Coordinates(Random.Range(0, _maze.Height), Random.Range(0, _maze.Width));
+        }
+
+        _tiles[enemyCoordinates1.X, enemyCoordinates1.Y].tileText.text = "F";
+
+        _pathFindingAlgorithm.FindPath(_maze, enemyCoordinates, enemyCoordinates1, out List<Coordinates> path);
+        foreach (var coordinate in path)
+        {
+            _tiles[coordinate.X, coordinate.Y].tileText.text += "X";
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 }
